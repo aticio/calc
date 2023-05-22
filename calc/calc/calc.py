3e172f914878b5f560aff75cd0e6d497337c9ddd
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-05-22 14:41:15 +0000

    step-3-adding-multiple-numbers

--- calc/calc/calc.py
+++ calc/calc/calc.py
@@ -2,5 +2,5 @@
 
 
 class Calc:
-    def add(self, a, b):
-        return 9
+    def add(self, *args):
+        return sum(args)
--- calc/tests/test_calc.py
+++ calc/tests/test_calc.py
@@ -7,8 +7,22 @@ import pytest
 
 from calc.calc import Calc
 
+
 def test_add_two_numbers():
     c = Calc()
 
     res = c.add(4, 5)
     assert res == 9
+
+
+def test_add_three_numbers():
+    c = Calc()
+
+    res = c.add(4, 5, 6)
+
+    assert res == 15
+
+
+def test_add_many_numbers():
+    s = range(100)
+    assert Calc().add(*s) == 4950
