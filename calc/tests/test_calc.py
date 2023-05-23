64c89c35c988c21c270c08847da1961c1ba435e5
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-05-23 09:04:48 +0000

    step-4-subtraction

--- calc/tests/test_calc.py
+++ calc/tests/test_calc.py
@@ -26,3 +26,10 @@ def test_add_three_numbers():
 def test_add_many_numbers():
     s = range(100)
     assert Calc().add(*s) == 4950
+
+
+def test_subtract_two_numbers():
+    c = Calc()
+
+    res = c.sub(10, 3)
+    assert res ==7
